35fba97f16119ba6cede318792d205709552af2f
Aphixyz <650112418030@bru.ac.th>
2025-01-28 18:41:07 +0700

    formAdminMangeTrainerlist

---
+++ app/member/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <div className='flex items-center justify-center h-screen'>
+        <h1> Page Member List</h1>
+      </div>
+    </div>
+  )
+}
+
+export default page
